cb8ae5f45da148c54797177ba5063ee1c324731b
LucasEdu10 <lucas.carvalho.luz1@gmail.com>
2025-05-12 01:04:58 -0300

    Added archives of test in vps server

--- the-portfolio/src/App.jsx
+++ the-portfolio/src/App.jsx
@@ -2,32 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import giffGatinho from './assets/gatinho_coracao.gif'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>Bommm dia meu amor</h1>
+      <h1>EU AMO VOCÊ! ❤️</h1>
+      <h2>
+        <img src={giffGatinho} alt='Gatinho Coração' />
+      </h2>
+    </div>
     </>
   )
 }
